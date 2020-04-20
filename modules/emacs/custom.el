241f756327309245209988b206d2d9061b0d48a1
Volodymyr Kononenko <vmkononenko@gmail.com>
2020-04-20 11:50:10 +0300

    [emacs] Save customizations inside repository

---
+++ modules/emacs/custom.el
@@ -0,0 +1,19 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(c-basic-offset 4)
+ '(custom-enabled-themes (quote (misterioso)))
+ '(package-check-signature nil)
+ '(package-selected-packages
+   (quote
+    (gnu-elpa-keyring-update iedit anzu comment-dwim-2 ws-butler dtrt-indent clean-aindent-mode yasnippet undo-tree volatile-highlights helm-gtags helm-projectile helm-swoop helm zygospore projectile company use-package default-text-scale))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- modules/emacs/init.el
+++ modules/emacs/init.el
@@ -1,3 +1,6 @@
+(setq custom-file "~/.my-linux-env/modules/emacs/custom.el")
+(load custom-file)
+
 (load "~/.emacs.d/emacs-c-ide-demo/init.el")
 
 ;; emacs-c-ide configuration disables menu bar, let's reenable it here
@@ -7,26 +10,6 @@
 
 (add-to-list 'package-archives
              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(c-basic-offset 4)
- '(custom-enabled-themes (quote (misterioso)))
- '(package-check-signature nil)
- '(package-selected-packages
-   (quote
-    (gnu-elpa-keyring-update iedit anzu comment-dwim-2 ws-butler dtrt-indent clean-aindent-mode yasnippet undo-tree volatile-highlights helm-gtags helm-projectile helm-swoop helm zygospore projectile company use-package default-text-scale))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 
 (when (version<= "26.0.50" emacs-version )
   (global-display-line-numbers-mode))
